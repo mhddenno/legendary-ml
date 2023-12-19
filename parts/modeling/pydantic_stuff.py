c488b0510fcc9a39a48fa283137508c9b97a5884
Mohamad Denno <dennokhmhd@gmail.com>
2023-12-19 15:24:09 +0100

    WIP pydantic

--- parts/modeling/pydantic_stuff.py
+++ parts/modeling/pydantic_stuff.py
@@ -3,7 +3,8 @@ from typing import Optional, List
 import json
 import logging
 import pdb
-
+from pydantic_core import PydanticCustomError
+from icecream import ic
 
 class ISBN10AndISBN13MissingError(Exception):
     def __init__(self, title:str, message:str) -> None:
@@ -20,8 +21,11 @@ class ISBN10FormatError(Exception):
 
 def process_topic(topic: dict) -> Optional[dict]:
     try:
+        ic()
         return Topic(**topic)
     except pydantic.ValidationError:
+        #pdb.set_trace()
+        ic(f"Processing topic: {topic['title']} didn't work.")
         logging.error(f"Processing topic: {topic['title']} didn't work.")
         return None
 
@@ -38,20 +42,41 @@ class Topic(pydantic.BaseModel):
     price: Optional[float]
     co_author: Optional[Co_author]
 
-
-    @pydantic.model_validator(mode='before')
+    @pydantic.model_validator(mode='after')
     def check_if_both_ibsn_exist(self) -> None:
-        pdb.set_trace()
-        if not self["isbn_10"] and not self["isbn_13"]:
-            raise ISBN10AndISBN13MissingError('Something', "Both ISBN10 and ISBN13 are missing.")
+        ic()
+        try:
+            if not hasattr(self,'isbn_10') or not hasattr(self, 'isbn_13'):
+                raise ISBN10AndISBN13MissingError('Something', "Both ISBN10 and ISBN13 are missing.")
+        except ISBN10AndISBN13MissingError as s:
+            ic('check_if_both_ibsn_exist')
+        return self
+        
+    @pydantic.model_validator(mode='before')
+    @classmethod
+    def check_if_title_is_so_long(cls, values: any) -> None:
+        ic()
+        try:
+            if len(values['title']) > 15:
+                raise PydanticCustomError('Title is too long','sdf')
+        except:
+            ic('check_if_title_is_so_long')
+        return values
+
+
+
 
 def main() -> None:
     data_path = "data/modeling.json"
+    ic()
     with open(data_path, "r") as f:
         data = json.load(f)
         #[print(topic) for topic in data]
-        topics: List[Topic] = [Topic(**topic) for topic in data if process_topic(topic) is not None]
-        
+        topics = [Topic(**topic) for topic in data if process_topic(topic) is not None] 
+        #topics=[]
+        #for topic in data:
+        #    topics.append(Topic(**topic))
+
         #print(Topic.__pydantic_decorators__)
 
 if __name__ == "__main__":
