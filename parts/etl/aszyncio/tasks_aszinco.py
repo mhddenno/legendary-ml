ab97876c42f6be0ba2c97221e2e7153d8d398f75
Mohamad Denno <dennokhmhd@gmail.com>
2023-12-29 09:12:40 +0100

    adding some explanation

--- parts/etl/aszyncio/tasks_aszinco.py
+++ parts/etl/aszyncio/tasks_aszinco.py
@@ -1,3 +1,12 @@
+"""
+Concurrency: Concurrency is about making progress on multiple tasks, but not necessarily at the same instant. In the context of asyncio, it's achieved through asynchronous programming using coroutines and event loops. While one task is waiting for I/O (e.g., reading from a file or making a network request), the event loop can switch to another task, allowing the program to make progress on multiple activities.
+
+Parallelism: Parallelism involves simultaneously executing multiple tasks at the same time, typically on multiple processors or cores. This is achieved using parallel programming techniques like multiprocessing. Each task runs independently and may execute in parallel, providing potential speedup for CPU-bound tasks.
+
+asyncio is not designed for parallelism in the traditional sense. It is more suitable for I/O-bound tasks where tasks spend a significant amount of time waiting for external resources (like files, network requests, or databases). While one task is waiting, the event loop can switch to another task, making the overall program more efficient.
+"""
+
+
 import asyncio
 
 async def task1() -> None:
